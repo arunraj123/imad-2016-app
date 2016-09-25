be6a79fab0dc059c9794d2d9cb27d6551d97ebda
arunraj123 <arun.raj1897@gmail.com>
2016-09-25 09:28:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res)){
-   res.send(this article is being served here);
+   res.send(article one is being served here);
 });
 app.get('/article-two',function(req,res)){
-   res.send(this article is being served here);
+   res.send( article two is being served here);
 });
 app.get('/article-three',function(req,res)){
-   res.send(this article is being served here);
+   res.send(article three is being served here);
 });
 
 app.get('/ui/style.css', function (req, res) {
