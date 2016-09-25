5fd5cb3cf821e23a5ff2d245588f366d5b2ce7e8
arunraj123 <arun.raj1897@gmail.com>
2016-09-25 13:17:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-
+var content ={
+    title="ARUN ARTILCE";
+    heading: "love";
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
