040ee327a1b9dd2603352f3c95051872bf4e0a48
arunraj123 <arun.raj1897@gmail.com>
2016-12-15 22:06:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 var content ={
-    title="ARUN ARTILCE";
+    title:"ARUN ARTILCE";
     heading: "love";
 }
 app.get('/', function (req, res) {
